7ab9d27a815ac15e510909b31f5c646b284763ea
d1gi <digital_boy@mail.ru>
2015-10-30 20:10:18 +0700

    add PDOException

--- ajax_cities.php
+++ ajax_cities.php
@@ -5,14 +5,20 @@ $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ];
-$pdo = new PDO($dsn, 'dynamic_form', '1234', $opt);
+
+try {
+    $pdo = new PDO($dsn, 'dynamic_form', '1234', $opt);
+} catch (PDOException $e) {
+    die('Подключение не удалось: '.$e->getMessage());
+}
 
 $search = strip_tags(trim($_GET['q']));
 $stmt = $pdo->prepare('SELECT * FROM cities WHERE name LIKE :search LIMIT 30');
 $stmt->execute([':search' => $search.'%']);
 
-foreach ($stmt as $row)
-{
+$data = [];
+
+foreach ($stmt as $row) {
     $data[] = [
         'id'   => $row['id'],
         'name' => $row['name']

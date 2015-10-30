9c0a2e46c32f6e4903a5a53bcc53acfcbf2c5f36
d1gi <digital_boy@mail.ru>
2015-10-30 20:10:47 +0700

    fix

--- ajax_cities.php
+++ ajax_cities.php
@@ -1,6 +1,6 @@
 <?php
 
-$dsn = "mysql:host=127.0.0.1;dbname=dynamic_form;charset=uft-8";
+$dsn = "mysql:host=127.0.0.1;dbname=dynamic_form;charset=uft8";
 $opt = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
